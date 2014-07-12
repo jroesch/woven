d6f0092fd21cd5d06ff4e48ff621f78638c6dea2
Jared Roesch <roeschinc@gmail.com>
2014-07-11 21:07:05 -0700

    commit changes before major work

--- future.rb
+++ future.rb
@@ -1,19 +1,42 @@
-# Better interface to deferrables from EM
-#
-#
-class Result; end
-class Success < Result; end
-class Failure < Result; end
+require 'em-synchrony'
+require 'fiber'
+
+class Result
+  def success?
+    !self.failed?
+  end
+
+  def failure?
+    !self.success?
+  end
+end
+
+class Success < Result
+  def success?
+    true
+  end
+end
+
+class Failure < Result
+  def failure?
+    true
+  end
+end
 
 class Awaitable; end
 
 class Promise
   attr_reader :future
+  
+  def initialize
+    @result = nil
+  end
 
   def self.fail(error)
     if error.is_a?(StandardError)
-      raise TypeError, "A Promise must fail with a subtype of StandardError"
+      raise "Not Yet!"
     else
+      raise TypeError, "A Promise must fail with a subtype of StandardError"
     end
   end
 
@@ -24,26 +47,36 @@ class Promise
   def complete(result)
   end
 
-  def success(value)
+  def succeed_with(value)
     complete(Success.new(cause))
   end
 
-  def failure(cause)
+  def fail_with(cause)
     complete(Failure.new(cause))
   end
 
+  def succeed?
+  end
+
+  def failed?
+  end
+
   def on_complete
-    yield 1
+    yield @result
+  end
+  
+private
+  # I think this is the MVP but we should replace this with an Executor/ExecutionContext like abstraction
+  # for scheduling work across threads and fibers, more thought is needed here
+  
+  def check_progress(original_fiber)
+    if finished? && original_fiber.alive? && original_fiber != Fiber.current && @yielded
+      original_fiber.resume
+    end
+  end
+
+  def finished?
+    !@result.nil?
   end
 end
 
-
-p = Promise.new
-p.on_complete do |result|
-  case result
-  when Success
-    result.value
-  when Failure
-    result.error
-  end
-end
