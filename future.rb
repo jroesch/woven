7ff6266c812597cb003eac559abd2a6d2fbf1b4b
Jared Roesch <roeschinc@gmail.com>
2014-07-11 23:59:02 -0700

    More experiments with design of both Future/Promises and ExecutionContexts

--- future.rb
+++ future.rb
@@ -1,28 +1,6 @@
 require 'em-synchrony'
 require 'fiber'
 
-class Result
-  def success?
-    !self.failed?
-  end
-
-  def failure?
-    !self.success?
-  end
-end
-
-class Success < Result
-  def success?
-    true
-  end
-end
-
-class Failure < Result
-  def failure?
-    true
-  end
-end
-
 class Awaitable; end
 
 class Promise
@@ -30,6 +8,8 @@ class Promise
   
   def initialize
     @result = nil
+    @yielded = false
+    @future = Future.new(self)
   end
 
   def self.fail(error)
@@ -44,39 +24,132 @@ class Promise
     raise "not yet implemented"
   end
 
-  def complete(result)
+  def fulfill(result)
+    @result = result 
   end
 
-  def succeed_with(value)
-    complete(Success.new(cause))
-  end
-
-  def fail_with(cause)
-    complete(Failure.new(cause))
-  end
-
-  def succeed?
+  def success?
+    !failed?
   end
 
   def failed?
+    @result.nil?
   end
 
   def on_complete
     yield @result
   end
-  
+
+  def value
+    unless fulfilled?
+      Fiber.yield
+    end
+    
+    case @result
+    when StandardError
+      raise @result
+    else
+      @result
+    end
+  end
+
 private
   # I think this is the MVP but we should replace this with an Executor/ExecutionContext like abstraction
   # for scheduling work across threads and fibers, more thought is needed here
   
-  def check_progress(original_fiber)
-    if finished? && original_fiber.alive? && original_fiber != Fiber.current && @yielded
-      original_fiber.resume
+  # def check_progress(original_fiber)
+  #  if fulfillled? && original_fiber.alive? && original_fiber != Fiber.current && @yielded
+  #    original_fiber.resume
+  #  end
+  # end
+
+  def fulfilled?
+    !@result.nil?
+  end
+end
+
+module Scheduler
+  class FIFO
+  end
+end
+
+# I think this may be wrong headed, need to sleep on it
+class ExecutionContext
+  def initialize(root_fiber, backing, scheduling)
+    @root_fiber = root_fiber
+    
+    case backing
+    when :fibers
+      use_scheduling!(scheduling)
+    else
+      raise ArgumentError, "unsupported execution model: #{backing}"
+    end
+  end
+  
+  def execute(task)
+    @queue.empty?
+    resume_scheduler
+  end
+  
+  def run
+    while !@queue.empty?
+      task = @queue.pop
+      task.transfer
+      if task.alive?
+        @queue << task
+      end
     end
   end
 
-  def finished?
-    !@result.nil?
+  def use_scheduling!(scheduling)
+    @scheduler = 
+      case scheduling
+      when :fifo
+      else
+        raise ArgumentError, "unsupported scheduler"
+      end
   end
+
+private
+  def resume_scheduler
+    Fiber.current != @root_fiber
+    @root_fiber.transfer
+  end
+end
+
+class Future
+  def self.all(*args)
+    future { args.map { |arg| arg.value } }
+  end
+
+  def initialize(promise, &body)
+    @promise = promise
+    @body = body
+  end
+
+  # Kick off Fiber then return a reference to itself?
+  def run
+    promise = Promise.new 
+    if @body 
+      Fiber.new do
+        begin
+          result = @body.call
+          promise.fulfill(result)
+        rescue StandardError => e
+          promise.fulfill(e)
+        end 
+      end.resume
+    end 
+
+    promise.future
+  end
+
+  def value
+    @promise.value
+  end
+end
+
+def future(&body)
+  Future.new(Promise.new, &body).run
 end
 
--- test.rb
+++ test.rb
@@ -2,15 +2,73 @@ require './future'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
+require 'pry'
 
 describe "A Promise" do
-  it "should be completed when created using Promise#succeed" do; end
+  it "should be completed when created using Promise#succeed" do
+    skip "nope" 
+  end
 
-  it "should be failed when created using Promise#fail" do; end
+  it "should be failed when created using Promise#fail" do
+    skip "not yet" 
+  end
   
   it "should provide access to a future which it completes" do
-    promise = Promise.succeed(10)
-    promise.future
-    raise "Not sure what comes next"
+    # promise = Promise.succeed(10)
+    # promise.future
+    skip "Not sure what comes next"
+  end
+end
+
+describe "ExecutionContext" do
+  setup do
+    @ec = ExecutionContext.new(:fibers, :fifo)
+  end
+
+  teardown do
+    @ec.clear!
+  end
+
+  it "should execute a task" do
+    @ec.execute()
+  end
+end
+
+describe "A Future" do
+  it "should run and complete" do
+    f = future do
+      1
+    end
+
+    f.value.must_equal(1)
+  end
+  
+
+  it "#all method should sequence a set of futures" do
+    Future.all(future { 1 }, future { 2 }, future { 3 }).value.must_equal [1,2,3]
+  end
+
+  it "should create be able to create and user a future via `future`" do
+    skip "foo"
+    ordering = []
+    # set them up to execute in reverse order
+    one = future do
+      Fiber.yield
+      ordering << 1
+    end
+
+    two = future do
+      Fiber.yield
+      ordering << 2
+    end
+
+    three = future do
+      ordering << 3
+    end
+    
+    while ordering.length < 3; end
+    
+    # execute them in order, if we aren't using fibers each being evaluated in order will cause them to sequentially evaluate with the sleeps
+    ordering.must_equal([3,2,1])
   end
 end
